7ae747c10bc2df77b43328718c7858395934306f
lvterry <lvterry@gmail.com>
2020-12-08 23:05:46 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React from "react";
 import "./style.css";
 
+function Header(props) {
+  return (
+    <header>
+      <h1>{props.name}'s Kitchen</h1>
+    </header>
+  );
+}
+
+function Main(props) {
+  return (
+    <section>
+      <p>We offer the best food.</p>
+      <ul>
+        {props.dishes.map(dish => (
+          <li key={dish.id}>{dish.name}</li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+function Footer(props) {
+  return <footer>Copyright {props.year}</footer>;
+}
+
+const dishes = ["pizza", "pasta", "sandwiches", "burger"];
+const dishObjects = dishes.map((dish, i) => ({ id: i, name: dish }));
+
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <>
+      <Header name="Terry" />
+      <Main dishes={dishObjects} />
+      <Footer year={new Date().getFullYear()} />
+    </>
   );
 }
